2761868718208f50e63b6734f5486163c84f2548
Lucas <v092953@tafe.wa.edu.au>
2024-10-01 12:14:04 +0800

    week8

---
+++ week8/binary_search_tree.py
@@ -0,0 +1,15 @@
+class BinarySearchTree:
+
+    def __init__(self, data=None) -> None:
+        self.parent = parent
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def in_order_traversal(root):
+
+        if root is None:
+            return
+        
+    def left_traversal(self, root):
+        root.left
